46f8fc3db202b2423f44789d38dde7037f6a001a
Kyle Wells <47221196+kqwq@users.noreply.github.com>
2023-11-21 14:52:57 -0500

    static url github

--- src/routes/api/content/+server.js
+++ src/routes/api/content/+server.js
@@ -1,27 +1,35 @@
-import fs from 'fs';
-import {parse} from 'csv-parse';
-import { json } from '@sveltejs/kit';
+// import fs from "fs";
+import { parse } from "csv-parse";
+import { json } from "@sveltejs/kit";
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {
-	const comp_file = String(url.searchParams.get('file'))
-	let location = `./static/${comp_file}`;
-	const data = await new Promise(resolve => {
-		const csv_data = [];
-		fs.createReadStream(location)
-			.pipe(parse())
-			.on('data', row => {
-				csv_data.push({
-					name: row[1],
-					problems: row.slice(6).map(s => {
-						const match = s.match(/(\d+)\n(\d+) min/);
-						if (match)
-							return {attempts: parseInt(match[1]), time: parseInt(match[2])};
-						return undefined;
-					})
-				})
-			})
-			.on('end', () => resolve(csv_data));
-	})
-	return json(data);
+  // const comp_file = String(url.searchParams.get("file"));
+  // let location = `./static/${comp_file}`;
+
+  const static_url =
+    "https://raw.githubusercontent.com/wzid/kattis-scoreboard-replay/main/static/rmc2023.csv";
+
+  const csv_data = [];
+  const res = await fetch(static_url);
+  const text = await res.text();
+
+  const data = await new Promise((resolve) => {
+    parse(text)
+      .on("data", (row) => {
+        // row = row.split(",");
+        csv_data.push({
+          name: row[1],
+          problems: row.slice(6).map((s) => {
+            const match = s.match(/(\d+)\n(\d+) min/);
+            if (match)
+              return { attempts: parseInt(match[1]), time: parseInt(match[2]) };
+            return undefined;
+          }),
+        });
+      })
+      .on("end", () => resolve(csv_data));
+  });
+
+  return json(data);
 }
